400ee1dde7473f963e74fc7c6dad0ff9101e79ea
Lewis Milburn <63267144+lewmilburn@users.noreply.github.com>
2024-06-02 11:09:23 +0100

    Add lang

--- Processes/DefaultViews/Page.php
+++ Processes/DefaultViews/Page.php
@@ -4,7 +4,7 @@ $DBMS = new DBMS();
 $Page = $DBMS->Query("SELECT * FROM `".DB_PREFIX."pages` WHERE `url` = '".$DBMS->Escape($_SERVER['REQUEST_URI'])."';");
 $PageData = $Page->fetch_object();
 ?><!DOCTYPE html>
-<html>
+<html lang="<?= WEBSITE_LANGUAGE; ?>">
     <head>
         <title><?= $PageData->title; ?></title>
     </head>
